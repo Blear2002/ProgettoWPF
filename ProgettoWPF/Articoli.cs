2f24232bdb7c7036eced435371bf220c66848de8
Antonio Tucci <antonio@tucci.com>
2021-10-15 14:33:45 +0200

    Classe articolo e metodo info() #4

---
+++ ProgettoWPF/Articoli.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoWPF
+{
+    class Articoli
+    {
+        int _codice;
+        string _descrizione;
+        double _prezzo;
+
+        public int Codice
+        {
+            get
+            {
+                return _codice;
+            }
+            set { _codice = value; }
+        }
+        public string Descrizione
+        {
+            get
+            {
+                return _descrizione;
+            }
+            set { _descrizione = value; }
+        }
+        public double Prezzo
+        {
+            get
+            {
+                return _prezzo;
+            }
+            set { _prezzo = value; }
+        }
+
+        public Articoli(int codice, string descrizione, double prezzo)
+        {
+            _codice = codice;
+            _descrizione = descrizione;
+            _prezzo = prezzo;
+        }
+        
+        public void Info()
+        {
+            Console.WriteLine("L'articolo " + _descrizione + " di codice " + _codice + " ha un prezzo di " + _prezzo + "$");
+        }
+
+        
+    }
+}
